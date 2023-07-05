664a49787d07038e088c10823a98417c7c6a753b
Deivid Almeida <mr.davialmeida@hotmail.com>
2023-07-04 23:04:51 -0300

    :construction: building event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from tkinter import *
-from  tkinter import ttk
+from home import set_puslisher_table 
 
 def get_puslisher():
   url = "http://18.117.168.254/publisher"
@@ -9,41 +9,15 @@ def get_puslisher():
 
   return requests.request("GET", url, headers=headers, data=payload)
 
-def set_puslisher_table(response):
-  frame = Frame(window)
-  frame.pack()
-  table = ttk.Treeview(frame)
-  table["columns"] = ("id", "name", "type", "gender", "amount")
-
-  table.column("#0", width=0,  stretch=NO)
-  table.column("id",anchor=CENTER, width=80)
-  table.column("name",anchor=CENTER,width=160)
-  table.column("type",anchor=CENTER,width=80)
-  table.column("gender",anchor=CENTER,width=80)
-  table.column("amount",anchor=CENTER,width=80)
-
-  table.heading("#0",text="",anchor=CENTER)
-  table.heading("id",text="Id",anchor=CENTER)
-  table.heading("name",text="Nome",anchor=CENTER)
-  table.heading("type",text="Tipo",anchor=CENTER)
-  table.heading("gender",text="Gênero",anchor=CENTER)
-  table.heading("amount",text="Qtd",anchor=CENTER)
-
-  i=0
-  for element  in response.json():
-    table.insert(parent="",index="end",iid=i,text="",
-    values=(element["id"], element["name"],element["type"],element["gender"], element["amount"]))
-    i+=1
-
-  table.pack()
-
 
 window = Tk()
 window.title("Designações")
-window.geometry("500x500")
+window.geometry("{0}x{1}+0+0".format(window.winfo_screenwidth(), window.winfo_screenheight()))
 window["bg"] = "#AC99F2"
+frame = Frame(window)
+frame.pack()
 
 res = get_puslisher()
-set_puslisher_table(res)
+set_puslisher_table(res, frame)
 
 window.mainloop()
