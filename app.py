142435d18018fe225b751eacc7b3329383e67659
Deivid Almeida <mr.davialmeida@hotmail.com>
2023-08-31 18:23:39 -0300

    :sparkles: add create and delete publisher

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import requests
 import flet as ft
 import json
-publisher_id = None
-rows = []
 
-def get_puslisher():
+def get_puslishers():
   url = "http://18.117.168.254/publisher"
   payload = {}
   headers = {}
   return requests.request("GET", url, headers=headers, data=payload)
 
-def patch_puslisher(data):
+def get_puslisher(publisher_id):
+  url = "http://18.117.168.254/publisher/{}".format(publisher_id)
+  payload = {}
+  headers = {}
+  return requests.request("GET", url, headers=headers, data=payload)
+
+def patch_puslisher(data, publisher_id):
   url = "http://18.117.168.254/publisher/{}".format(publisher_id)
   payload = json.dumps(data)
 
@@ -19,117 +23,242 @@ def patch_puslisher(data):
   }
   return requests.request("PATCH", url, headers=headers, data=payload)
 
+def post_puslisher(data):
+  url = "http://18.117.168.254/publisher"
+  payload = json.dumps(data)
+
+  headers = {
+    'Content-Type': 'application/json'
+  }
+  return requests.request("POST", url, headers=headers, data=payload)
+
+def delete_puslisher(publisher_id):
+  url = "http://18.117.168.254/publisher/{}".format(publisher_id)
+  payload = {}
+  headers = {}
+
+  return requests.request("DELETE", url, headers=headers, data=payload)
+
 def main(page: ft.Page):
-  table = ft.DataTable(
-    columns=[
-      ft.DataColumn(ft.Text("Id")),
-      ft.DataColumn(ft.Text("Nome")),
-      ft.DataColumn(ft.Text("Tipo")),
-      ft.DataColumn(ft.Text("Gênero")),
-      ft.DataColumn(ft.Text("Qtd")),
-      ft.DataColumn(ft.Text("Ativo")),
-    ],
-    rows=rows,
-  )
-  name = ft.TextField(label="Nome")
-  type = ft.Dropdown(
-    label="Tipo",
-    options=[
-        ft.dropdown.Option(1, "Novato"),
-        ft.dropdown.Option(2, "Intermediário"),
-        ft.dropdown.Option(3, "Experiente"),
-    ],
-  )
 
-  gender = ft.Dropdown(
-    label="Gênero",
-    options=[
-      ft.dropdown.Option("male", "Masculino"),
-      ft.dropdown.Option("female", "Feminino"),
-    ],
-  )
+  def close_dlg(e):
+    page.dialog.open = False
+    page.update()
 
-  active = ft.Switch(label="Status", value=False)
+  def delete(id):
+    res = delete_puslisher(id)
+    if res.json() == 1:
+      close_dlg(None)
+      page.go("/")
+
+  page.title = "Routes Example"
   saved_alert = ft.AlertDialog(title=ft.Text("Alterações salvas!"))
 
   def set_table():
-    global rows
-    rows = []
-    data = get_puslisher()
-    for element  in data.json():
-      rows.append(ft.DataRow(
-        cells=[
-          ft.DataCell(ft.Text(element['id'])),
-          ft.DataCell(ft.Text(element['name'])),
-          ft.DataCell(ft.Text(element['type'])),
-          ft.DataCell(ft.Text(element['gender'])),
-          ft.DataCell(ft.Text(element['amount'])),
-          ft.DataCell(ft.Checkbox(label="",value=element['active'], disabled=True)),
-        ],
-        selected=True,
-        data=element,
-        on_select_changed=lambda e: set_values(e.control.data),
-      ))
-    table.rows = rows
-    page.update()
+      rows = []
+      data = get_puslishers()
+      for element  in data.json():
+        rows.append(
+          ft.DataRow(
+            cells=[
+              ft.DataCell(ft.Text(element['id'])),
+              ft.DataCell(ft.Text(element['name'])),
+              ft.DataCell(ft.Text(element['type'])),
+              ft.DataCell(ft.Text(element['gender'])),
+              ft.DataCell(ft.Text(element['amount'])),
+              ft.DataCell(ft.Checkbox(label="",value=element['active'], disabled=True)),
+            ],
+            selected=True,
+            data=element,
+            on_select_changed=lambda e: page.go("/publisher/{}".format(e.control.data["id"])),
+          )
+        )
+      return rows
 
-  
-  def init():
-    set_table()
-    page.add(
-      ft.Row([
-        ft.Container(
-          table,
-        ),
-        ft.Column([
-          ft.Container(
-            name,
-          ),
-          ft.Container(
-            type,
-          ),
-          ft.Container(
-            gender,
-          ),
-          ft.Container(
-            active,
-          ),
-          ft.Container(
-            ft.ElevatedButton("Salvar", on_click=lambda e: save()),
-          ),
-        ]),
-      ]),
-      saved_alert,
-    )
+  def route_change(route):
+      troute = ft.TemplateRoute(page.route)
+      page.views.clear()
+      page.views.append(
+        ft.View(
+          "/",
+          [
+            ft.AppBar(title=ft.Text("Lista de publicadores"), bgcolor=ft.colors.SURFACE_VARIANT),
+            ft.ElevatedButton("Adicionar Publicador", on_click=lambda _: page.go("/publisher")),
+            ft.DataTable(
+              columns=[
+                ft.DataColumn(ft.Text("Id")),
+                ft.DataColumn(ft.Text("Nome")),
+                ft.DataColumn(ft.Text("Tipo")),
+                ft.DataColumn(ft.Text("Gênero")),
+                ft.DataColumn(ft.Text("Qtd")),
+                ft.DataColumn(ft.Text("Ativo")),
+              ],
+              rows=set_table(),
+            ),
+          ],
+        )
+      )
+      
+      if troute.match("/publisher"):
+        name = ft.TextField(label="Nome")
+        type = ft.Dropdown(
+          label="Nível",
+          options=[
+            ft.dropdown.Option(1, "Novato"),
+            ft.dropdown.Option(2, "Intermediário"),
+            ft.dropdown.Option(3, "Experiente"),
+          ],
+        )
+        gender = ft.Dropdown(
+          label="Gênero",
+          options=[
+            ft.dropdown.Option("male", "Masculino"),
+            ft.dropdown.Option("female", "Feminino"),
+          ],
+        )
+        active = ft.Switch(label="Status")
+        page.views.append(
+          ft.View(
+            "/publisher",
+            [
+              ft.AppBar(title=ft.Text("Adicionar publicador"), bgcolor=ft.colors.SURFACE_VARIANT),
+              ft.Row([
+                ft.Column([
+                  ft.Container(
+                    name,
+                  ),
+                  ft.Container(
+                    type,
+                  ),
+                  ft.Container(
+                    gender,
+                  ),
+                  ft.Container(
+                    active,
+                  ),
+                  ft.Container(
+                    ft.ElevatedButton("Salvar", on_click=lambda e: save(
+                      {
+                        "name": name.value,
+                        "type": int(type.value),
+                        "gender": gender.value,
+                        "active": bool(active.value)
+                      }
+                    )),
+                  ),
+                ]),
+              ]),
+            ],
+          )
+        )
+      
+      if troute.match("/publisher/:id"):
+        puslisher = get_puslisher(troute.id).json()
+        name = ft.TextField(label="Nome", value=puslisher["name"])
+        type = ft.Dropdown(
+          label="Nível",
+          value=puslisher["type"],
+          options=[
+            ft.dropdown.Option(1, "Novato"),
+            ft.dropdown.Option(2, "Intermediário"),
+            ft.dropdown.Option(3, "Experiente"),
+          ],
+        )
+        gender = ft.Dropdown(
+          label="Gênero",
+          value=puslisher["gender"],
+          options=[
+            ft.dropdown.Option("male", "Masculino"),
+            ft.dropdown.Option("female", "Feminino"),
+          ],
+        )
+        active = ft.Switch(label="Status", value=puslisher["active"])
+        page.views.append(
+          ft.View(
+            "/publisher/:id",
+            [
+              ft.AppBar(title=ft.Text(troute.id), bgcolor=ft.colors.SURFACE_VARIANT),
+              ft.Row([
+                ft.Column([
+                  ft.Container(
+                    name,
+                  ),
+                  ft.Container(
+                    type,
+                  ),
+                  ft.Container(
+                    gender,
+                  ),
+                  ft.Container(
+                    active,
+                  ),
+                  ft.Row([
+                    ft.ElevatedButton("Editar", on_click=lambda e: edit(
+                      {
+                        "name": name.value,
+                        "type": int(type.value),
+                        "gender": gender.value,
+                        "active": bool(active.value)
+                      },
+                      troute.id
+                    )),
+                    ft.ElevatedButton("Deletar", on_click=lambda e: open_dlg_modal(
+                      troute.id
+                    )),
+                  ]),
+                ]),
+              ]),
+            ],
+          )
+        )
+      
+      page.update()
 
-  def set_values(e):
-    global publisher_id
-    name.value = e['name']
-    type.value = e['type']
-    gender.value = e['gender']
-    active.value = e['active']
-    publisher_id = e['id']
-    page.update()
+  def view_pop(view):
+    page.views.pop()
+    if len(page.views) > 0:
+      top_view = page.views[-1]
+      page.go(top_view.route)
   
-  def save():
-    res = patch_puslisher({
-      "name": name.value,
-      "type": int(type.value),
-      "gender": gender.value,
-      "active": bool(active.value)
-    })
+  def edit(data, id):
+    res = patch_puslisher(data, id)
+    if res.json() == 1:
+      open_dlg()
+        
+  def save(data):
+    res = post_puslisher({
+      "name": data["name"],
+      "type": int(data["type"]),
+      "gender": data["gender"],
+      "active": bool(data["active"])
+      })
 
     if res.json() == 1:
       open_dlg()
-    
-    set_table()
-    page.update()
-
+  
   def open_dlg():
     page.dialog = saved_alert
     saved_alert.open = True
     page.update()
 
-  init()
+  def open_dlg_modal(id):
+    page.dialog = ft.AlertDialog(
+    modal=True,
+    title=ft.Text("Atenção!!"),
+    content=ft.Text("Tem certeza que deseja excluir?"),
+    actions=[
+        ft.TextButton("Não", on_click=lambda e: close_dlg(e)),
+        ft.TextButton("Sim", on_click= lambda e: delete(id)),
+    ],
+    on_dismiss=lambda e: print("Modal dialog dismissed!"),
+  )
+    page.dialog.open = True
+    page.update()
 
-ft.app(target = main)
+
+  page.on_route_change = route_change
+  page.on_view_pop = view_pop
+  page.go(page.route)
+
+
+ft.app(target=main, view=ft.AppView.FLET_APP)
